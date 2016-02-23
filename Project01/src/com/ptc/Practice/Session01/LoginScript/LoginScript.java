7c04c646f8426f44c06acd86f6dbd03539bd466e
Amit Humnabadkar <ahumnabadkar@ptc.com>
2016-02-23 17:52:59 +0530

    initial commit

---
+++ Project01/src/com/ptc/Practice/Session01/LoginScript/LoginScript.java
@@ -0,0 +1,49 @@
+package com.ptc.Practice.Session01.LoginScript;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class LoginScript {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new FirefoxDriver();
+        String appUrl = "https://accounts.google.com";
+        driver.get(appUrl);
+        driver.manage().window().maximize();
+
+        String expectedTitle = "Sign in - Google Accounts";
+        String actualTitle = driver.getTitle();
+        System.out.println("Actual Title: " + actualTitle);
+
+        if (actualTitle.contains(expectedTitle)) {
+            System.out.println("Testcase Passed");
+        } else {
+            System.out.println("Testcase failed");
+        }
+
+        WebElement username = driver.findElement(By.name("Email"));
+        username.clear();
+        username.sendKeys("jtest01412@gmail.com");
+
+        WebElement nextButton = driver.findElement(By.id("next"));
+        nextButton.click();
+
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        WebElement password = driver.findElement(By.id("Passwd"));
+        password.clear();
+        password.sendKeys("selenium001");
+
+        WebElement signInButton = driver.findElement(By.id("signIn"));
+        signInButton.click();
+
+        String title1 = driver.getTitle();
+        System.out.println("New Title: " + title1);
+	}
+
+}
